0732ee1e667c1a194291f39eae0d6bc79eb9815c
PalSvendsen <pal@getacademy.no>
2023-12-03 11:13:37 +0100

    Add project files.

---
+++ Card game/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace cardgame
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] cards = { "Clubs", "Hearts", "Spades", "Diamonds" };
+            Random random = new Random();
+            var randomIndex = random.Next(cards.Length);
+            Random Number = new Random();
+            var numIndex = random.Next(1, 11);
+            Random higher = new Random();
+            string[] high = { "Joker", "Ace", "King", "Queen" };
+            var highIndex = random.Next(high.Length);
+            string randomHigh = high[highIndex];
+            string randomCards = cards[randomIndex];
+           
+            Console.WriteLine($"Your random card is {numIndex} of {randomCards}. Your special card is {randomHigh}");
+
+        }
+    }
+} 
+
